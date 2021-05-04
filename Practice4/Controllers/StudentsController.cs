6b009082658d9827f3702fbb968f3c1b1d3b3570
Sofía Vargas <sofi@Sofias-MacBook-Air-1261.local>
2021-05-04 18:47:25 -0400

    P4-003 Added Files

---
+++ Practice4/Controllers/StudentsController.cs
@@ -0,0 +1,49 @@
+﻿using Logic_Layer.Managers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Logic_Layer.Models;
+using Logic_Layer.Managers;
+
+namespace Practice4.Controllers
+{
+    [ApiController]
+    [Route("[/api/students]")]
+    public class StudentsController : ControllerBase
+    {
+        private readonly IStudentManager _studentManager;
+        public StudentsController(IStudentManager studentManager)
+        {
+            _studentManager = studentManager;
+        }
+
+        [HttpPost]
+        public List<Student> CreatePerson([FromBody] Student studentName) // a. Create User
+        {
+            return _studentManager.CreatePerson(studentName);
+        }
+
+        [HttpPut]
+        public Student UpdateStudent([FromBody] Student student) // b. Update User
+        {
+            return _studentManager.UpdateStudent(student);
+        }
+
+        [HttpDelete]
+        public Student DeletePerson([FromBody] Student student) // c. Delete User
+        {
+            return _studentManager.DeletePerson(student);
+        }
+
+        [HttpGet]
+        public List<Student> GetStudents() // d. Get User
+        {
+            return _studentManager.GetAllStudents();
+        }
+
+    }
+}
--- Practice4/Startup.cs
+++ Practice4/Startup.cs
@@ -1,7 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Database_Layer;
+using Logic_Layer.Managers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -11,6 +9,12 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
 
 namespace Practice4
 {
@@ -26,12 +30,19 @@ namespace Practice4
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
+            services.AddTransient<IStudentManager, StudentManager>();
+            services.AddSingleton<IDbContext, DbContext>();
+            services.AddSwaggerGen(p =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Practice4", Version = "v1" });
+                p.SwaggerDoc("v3", new OpenApiInfo { Title = "Practice 4 WebAPI", Version = "v3"});
+
+                /*
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                p.IncludeXmlComments(xmlPath);
+                */
             });
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,8 +51,6 @@ namespace Practice4
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Practice4 v1"));
             }
 
             app.UseHttpsRedirection();
@@ -49,6 +58,13 @@ namespace Practice4
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(p =>
+            {
+                p.SwaggerEndpoint("/swagger/v3/swagger.json", "Practica 4");
+            });
 
             app.UseEndpoints(endpoints =>
             {
